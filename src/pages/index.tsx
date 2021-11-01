78458dfb2f47df15619b3ce6992ab354c01d5bed
Antonio Martin <tonitim13@gmail.com>
2021-11-01 17:52:27 -0300

    config eslint and prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,18 @@
-import type { NextPage } from "next";
-import Head from "next/head";
+import type { NextPage } from "next"
+import Head from "next/head"
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
         <title>My Blog</title>
-        <meta name="description" content="My blog"/>
-        <link rel="icon" href="/favicon.ico"/>
+        <meta name="description" content="My blog" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>My Blog</main>
-
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
